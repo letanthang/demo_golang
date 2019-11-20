480bb6e71714de7aed0834314a07f08947abb3e4
Le Tan Thang <letanthang@gmail.com>
2019-11-20 10:33:17 +0700

    type system

---
+++ type_system/main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type Student struct {
+	FirstName string `json:"first_name" bson:"full_name" validate:"required"`
+	LastName  string
+	Age       int
+	Email     string
+}
+
+func (s Student) GetFullName() string {
+	return s.FirstName + " " + s.LastName
+}
+
+func (s *Student) SetEmail(e string) {
+	s.Email = e
+}
+
+func main() {
+	fmt.Println("Hello nc student!")
+
+	// 	Int
+
+	aInt := 1
+	fmt.Println(aInt)
+
+	var bInt int
+	bInt = 2
+	fmt.Printf("bInt = %d\n", bInt)
+	// String
+
+	aString := "Hello abc"
+	fmt.Printf("aString = %s\n", aString)
+
+	// Array/Slice
+	aSlice := []string{"a", "b", "c"}
+	fmt.Println(aSlice)
+
+	bSlice := []int{1, 4, 6, 8, 8}
+	fmt.Println(bSlice)
+	fmt.Printf("%v\n", bSlice)
+
+	for _, v := range bSlice {
+		if v > 5 {
+			fmt.Printf("%d ", v)
+		}
+	}
+
+	aArray := [2]int{1, 2}
+	fmt.Println(aArray)
+
+	// Map
+	aMap := map[string]int{"age": 1000, "level": 5}
+	fmt.Println(aMap)
+
+	// Struct
+
+	// vic := Student{
+	// 	FirstName: "Victor",
+	// 	LastName:  "Nguyen",
+	// 	Age:       100,
+	// 	Email:     "victornguyen@gmail.com",
+	// }
+
+	vic := Student{"Victor", "Nguyen", 100, "hihihi"}
+
+	fmt.Println(vic)
+	fmt.Printf("%+v", vic)
+	// Interface
+	var i interface{}
+	i = vic
+	fmt.Println(i)
+
+	// Channel
+
+	ch := make(chan int, 2)
+	ch <- 10 // write to channel
+	ch <- 12 // write to channel
+
+	fmt.Println(<-ch) //read from channel
+	fmt.Println(<-ch) //read from channel
+
+	// Function
+
+	// function receiver
+
+	// pointer
+
+	fmt.Println(vic.GetFullName())
+	vic.SetEmail("hehehe")
+	SetEmail(&vic, "hahaha")
+
+	fmt.Println(vic.Email)
+
+	// int <-> string
+
+	cInt := 100
+
+	cString := fmt.Sprintf("%d hello", cInt)
+
+	cString = strconv.Itoa(cInt)
+	fmt.Println(cString)
+	// "100"
+
+	// parse inputJson to struct
+
+	// inputJson := `[
+	// 	{"first_name": "Victor", "last_name": "Nguyen", "age": 100, "class_name":"golang"},
+	// 	{"first_name": "Anh", "last_name": "Dinh", "age":200, "class_name":"golang"}
+	// 	]`
+
+	// fmt.Println(inputJson)
+}
+
+func SetEmail(s *Student, email string) {
+	s.Email = email
+}
