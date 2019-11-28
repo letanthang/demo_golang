7567f0c6799a3ee52ce9b65dbb66ad09dd6bb193
Le Tan Thang <letanthang@gmail.com>
2019-11-28 15:12:20 +0700

    hello grcp

--- hello_grpc/client/main.go
+++ hello_grpc/client/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"context"
 	"log"
-	"os"
-	"time"
 
 	pb "github.com/letanthang/demo_golang/hello_grpc/hellogrpc"
 	"google.golang.org/grpc"
@@ -20,25 +18,40 @@ func main() {
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
-
 	defer conn.Close()
+
 	c := pb.NewGreeterClient(conn)
-
-	name := defaultName
-
-	if len(os.Args) > 1 {
-		name = os.Args[1]
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-
-	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
-
+	resp, err := c.SayHello(context.TODO(), &pb.HelloRequest{Name: defaultName})
 	if err != nil {
-		log.Fatalf("cound not send greet: %v", err)
+		log.Fatalf("cound not send greeting: %v", err)
 	}
-
-	log.Printf("Response greeting: %s", r.Message)
-
+	log.Printf("Response greeting: %s", resp.Message)
 }
+
+// func main() {
+// 	conn, err := grpc.Dial(address, grpc.WithInsecure())
+// 	if err != nil {
+// 		log.Fatalf("did not connect: %v", err)
+// 	}
+
+// 	defer conn.Close()
+// 	c := pb.NewGreeterClient(conn)
+
+// 	name := defaultName
+
+// 	if len(os.Args) > 1 {
+// 		name = os.Args[1]
+// 	}
+
+// 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+// 	defer cancel()
+
+// 	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
+
+// 	if err != nil {
+// 		log.Fatalf("cound not send greet: %v", err)
+// 	}
+
+// 	log.Printf("Response greeting: %s", r.Message)
+
+// }
