aeef14a94f146e877e18c8343bf83a0aade6048f
Le Tan Thang <letanthang@gmail.com>
2019-11-30 13:48:12 +0700

    bench mutex

---
+++ benchmark_mutex/main_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"sync"
+	"testing"
+)
+
+func BenchmarkMutexLock(b *testing.B) {
+	m := sync.Mutex{}
+	for i := 0; i < b.N; i++ {
+		m.Lock()
+		m.Unlock()
+	}
+}
+func BenchmarkRWMutexLock(b *testing.B) {
+	m := sync.RWMutex{}
+	for i := 0; i < b.N; i++ {
+		m.Lock()
+		m.Unlock()
+	}
+}
+
+func BenchmarkRWMutexRLock(b *testing.B) {
+	m := sync.RWMutex{}
+	for i := 0; i < b.N; i++ {
+		m.RLock()
+		m.RUnlock()
+	}
+}
