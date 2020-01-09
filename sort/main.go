cc62fa95773f0de41562d736a327bdfe6a456949
Le Tan Thang <letanthang@gmail.com>
2020-01-09 15:47:02 +0700

    sort, reverse

---
+++ sort/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"sort"
+)
+
+func main() {
+	is1 := []int{3, 1, 8, 2}
+	is2 := make([]int, len(is1))
+	copy(is2, is1)
+	fmt.Println(is1, is2)
+	//sort way 1
+	sort.Slice(is1, func(i, j int) bool {
+		return is1[i] < is1[j]
+	})
+	fmt.Println(is1, is2)
+
+	//sort way 2
+	sort.Sort(sort.IntSlice(is2))
+	fmt.Println(is1, is2, reflect.DeepEqual(is1, is2))
+
+	//reverse
+	i := sort.Reverse(sort.IntSlice(is1))
+	sort.Sort(i)
+	fmt.Println(is1, is2)
+
+}
+
+func testArr() []int {
+	return nil
+}
