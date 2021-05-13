1982f3042509edd18a3bca02032a5d90a452bc33
Thang Le <thang.le@autonomous.nyc>
2021-05-13 09:40:28 +0700

    dev

--- substring2/main.go
+++ substring2/main.go
@@ -10,4 +10,7 @@ func main() {
 	// Take substring from index 4 to length of string.
 	substring := value[4:len(value)]
 	fmt.Println(substring)
+
+	//print value without ";"
+	fmt.Println(value[:3] + value[4:])
 }
