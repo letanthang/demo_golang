3a3755a4bdc0e57a7ce38d22f6f96f28c85329cf
Le Tan Thang <letanthang@gmail.com>
2022-09-27 16:01:13 +0700

    generic

--- generic/main.go
+++ generic/main.go
@@ -19,10 +19,24 @@ func Reverse[T any](slice []T) []T {
 	return result
 }
 
+type Number interface {
+	int | int64 | int32
+}
+
+func Min[T Number](x, y T) T {
+	if x < y {
+		return x
+	} else {
+		return y
+	}
+}
+
 func main() {
 	intSlice := []int{1, 4, 7, 9}
 	fmt.Println(intSlice)
 	fmt.Println("slice include", 9, Includes(intSlice, 9))
 	fmt.Println("reverse is", Reverse(intSlice))
+
+	fmt.Println("Min of 3 & 10 is", Min(3, 10))
 
 }
